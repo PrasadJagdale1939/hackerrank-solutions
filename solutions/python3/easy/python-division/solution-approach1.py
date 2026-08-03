e3894aa654990a7b10df98950c0db63c991bf6bd
Prasad Jagdale <prasadjagdaleofficial@gmail.com>
2026-08-03 10:18:07 +0530

    ✅ Python: Division [python3] — approach1

---
+++ solutions/python3/easy/python-division/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# Problem     Python: Division
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-03, 10:17 a.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a//b)
+    print(a/b)
