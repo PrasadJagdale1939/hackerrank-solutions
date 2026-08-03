fd848a6d438316ef796afbf690eea8af1f523e93
Prasad Jagdale <prasadjagdaleofficial@gmail.com>
2026-08-03 10:30:09 +0530

    ✅ Loops [python3] — approach1

---
+++ solutions/python3/easy/python-loops/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+# Problem     Loops
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-03, 10:30 a.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+for i in range(n):
+    print(i * i)
+
