7a4d69bce904898b5c0714b7e6f675687b9daeea
Prasad Jagdale <prasadjagdaleofficial@gmail.com>
2026-08-11 21:33:30 +0530

    ✅ Python If-Else [python3] — approach1

---
+++ solutions/python3/easy/py-if-else/solution-approach1.py
@@ -0,0 +1,33 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+# Problem     Python If-Else
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-11, 09:33 p.m.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+        
+    
+if n % 2 == 1:
+    print("Weird")
+elif 2 <= n <= 5:
+    print("Not Weird")
+elif 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
